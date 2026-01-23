30d12f7eb855488436718e6966a4409ceb5637a6
Sterling Duncan <techedreal@gmail.com>
2026-01-23 09:53:10 -0600

    cookie fix

--- Accounts/cookies.js
+++ Accounts/cookies.js
@@ -1,9 +1,7 @@
-function createLoginCookie(username){
-    current_date = new Date();
-    c = new Date(current_date.getTime() + 30*24*60*60*1000).toUTCString();
-
-    expiredate = new Date(current_date.getTime() - 1*24*60*60*1000).toUTCString();
-    document.cookie = "username=; expires=" + expiredate + "; path=/";
+function createLoginCookie(username) {
+    const current_date = new Date();
+    const expiredate = new Date(current_date.getTime() + 30*24*60*60*1000).toUTCString();
+    document.cookie = "username=" + encodeURIComponent(username) + "; expires=" + expiredate + "; path=/";
 }
 
 function checkLoginStatus() {
@@ -12,19 +10,17 @@ function checkLoginStatus() {
         if (name === 'username' && value) {
             updateUIForLoggedInUser(decodeURIComponent(value));
         }
-    }
+    });
 }
 
 function updateUIForLoggedInUser(username) {
-    // Attempt to find a sign in link/button in common locations
     const navbarLinks = document.querySelectorAll('#navbar a');
     navbarLinks.forEach(link => {
         if (link.href && link.href.includes('signin.html')) {
-            link.textContent = username; // Or 'Profile'
+            link.textContent = username;
             link.href = '#'; // Or link to a profile page
-                    }
+        }
     });
 }
 
 document.addEventListener('DOMContentLoaded', checkLoginStatus);
-return;
